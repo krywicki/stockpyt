df3770374dcb48233f32da33acf2b0d433a6eb02
Joe Krywicki <jkrywicki@yourdestinyapp.com>
2020-10-17 20:52:28 -0500

    Removed unneeded 'blessed' package from setup.py requires

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setuptools.setup(
     version="0.0.1dev",
     install_requires=[
         "requests~=2.24",
-        "rich~=8.0",
-        "blessed~=1.17"
+        "rich~=8.0"
     ],
     extras_require={
         "dev":["python-dotenv"]
