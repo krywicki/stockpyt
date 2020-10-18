2bbdeb3c4ed8dfcba496f8ec6822847a8c9a2879
Joe Krywicki <jkrywicki@yourdestinyapp.com>
2020-10-17 20:54:35 -0500

    Added custom min & max functions for yahoo finance ticker agent
    * Yahoo will sometimes send list of lists of number values back or will have 
       "NoneType" values interspersed with number values. Traditional min/max
       python builtin functions won't work.

--- stockpyt/tickers.py
+++ stockpyt/tickers.py
@@ -34,10 +34,39 @@ class YahooTickerAgent(TickerAgent):
         quote               = StockQuote()
         quote.symbol        = meta['symbol']
         quote.open          = indicators['open'][0]
-        quote.high          = max(indicators['high'])
-        quote.low           = min(indicators['low'])
+        quote.high          = self._max(indicators['high'])
+        quote.low           = self._min(indicators['low'])
         quote.prev_close    = meta['previousClose']
         quote.price         = meta['regularMarketPrice']
         quote.perc_change   = -((quote.prev_close - quote.price) / quote.prev_close) * 100.0
 
         return quote
+
+    def _max(self, values:list) -> float:
+        """ gets max value. accounts for list-of-lists """
+        val:float = 0.0
+        if not values:
+            return 0.0
+        elif isinstance(values[0], list):
+            values = [ self._max(vals) for vals in values ]
+
+        max_val = 0.0
+        for val in values:
+            if isinstance(val, int) or isinstance(val, float):
+                max_val = max(val, max_val)
+        return max_val
+
+    def _min(self, values:list) -> float:
+        """ gets max value. accounts for list-of-lists """
+        val:float = 0.0
+        if not values:
+            return 0.0
+        elif isinstance(values[0], list):
+            values = [ self._min(vals) for vals in values ]
+
+        min_val = 0.0
+        for val in values:
+            if isinstance(val, int) or isinstance(val, float):
+                min_val = max(val, min_val)
+        return min_val
+
