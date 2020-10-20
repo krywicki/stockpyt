e2ba727091e8c7358f915769ccd54cc809c7e8ab
Joe Krywicki <jkrywicki@yourdestinyapp.com>
2020-10-19 19:51:13 -0500

    Updated setup.py/cfg
    * Added reference to cli:main entry point for console_scripts
    * Added pythyon >= 3.8 requirement

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ import setuptools
 
 setuptools.setup(
     name="stockpyt",
-    version="0.0.1dev",
+    version="0.0.1",
+    python_requires = ">=3.8",
     install_requires=[
         "requests~=2.24",
         "rich~=8.0"
     ],
+    entry_points={
+        "console_scripts": [
+            "stockpyt=stockpyt.cli:main",
+        ],
+    },
     extras_require={
         "dev":["python-dotenv"]
     }
